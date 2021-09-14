e697f475bc93366c14e0d7c0f9379cd0bd013735
parth782 <parth11gupta@gmail.com>
2021-09-14 12:15:33 +0530

    alarm clock in js

---
+++ index.js
@@ -0,0 +1,52 @@
+console.log("hello");
+$('#success').hide();
+$('#danger').hide();
+var audio=new Audio("http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Sevish_-__nbsp_.mp3");
+let success=document.getElementById("success");
+let danger=document.getElementById("danger");
+
+const btnala=document.getElementById("setbtn");
+btnala.addEventListener("click",setalarm);
+function ringbell(){
+    audio.play();
+}
+function setalarm(e){
+    e.preventDefault();
+    
+    
+   
+    const alarm=document.getElementById("alarm");
+    const alarmdate=new Date(alarm.value);
+    console.log(`setting alarm for ${alarmdate}`);
+    
+
+    if(alarmdate=="Invalid Date"){
+        danger.classList.add('show');
+        $('#danger').show();
+        setTimeout(()=>{
+           $('#danger').hide();
+        },1500)
+    }
+    else{
+        success.classList.add('show');
+        $('#success').show();
+        setTimeout(()=>{
+           $('#success').hide();
+        },1500)
+    }
+    const now=new Date();
+    let timetoalarm=alarmdate-now;
+    console.log(timetoalarm);
+    
+    if(timetoalarm>=0){
+        setTimeout(()=>{
+           
+            ringbell();
+            
+        },timetoalarm)
+    }
+}
+let off=document.getElementById("setoff");
+off.addEventListener("click",()=>{
+    audio.stop()
+})
